9a12ca1bdf59f8859e4db2f16b24c84a62c5eb12
yoshimi <yoshi3923@icloud.com>
2020-12-03 18:03:30 +0900

    Add NanoEvery Program
    Add to the source code at NanoEvery.

---
+++ NanoEvery.ino
@@ -0,0 +1,25 @@
+void linkpulse(void){
+  for (int i=0;i<100;i++){
+    //PORTB=B01010101;
+    //PORTB=B10101010;       
+    VPORTB.OUT=B01010101;
+    VPORTB.OUT=B10101010;
+    delay(16);
+    }
+  }
+
+void setup(){
+//DDRB=B11111111;
+VPORTB.DIR=B11111111;
+////pinMode(2,INPUT_PULLUP);
+while(1){
+  linkpulse();
+  dot();
+  linkpulse();
+  space();
+  linkpulse();
+  dash();
+  }
+}
+void loop(){
+}
